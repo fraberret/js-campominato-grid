c89586fb3b7a2a80035807b0a57dfabfd42afcc1
fraberret <f.berretta02@gmail.com>
2024-02-28 18:09:11 +0100

    add comment on js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,84 +4,64 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 */
-
+/**in base alla scelta della difficolta, stampa una griglia
+ * 
+ * @param {string} difficult 
+ */
 function difficultChooser(difficult) {
 
     if (difficult === "easy") {
         document.getElementById('level').remove()
         document.querySelector('button').remove()
-    
-        document.getElementById('title').innerHTML="Difficoltà 1"
+
+        document.getElementById('title').innerHTML = "Difficoltà 1"
         for (let i = 1; i <= 100; i++) {
-            
+
             document.getElementById('container').insertAdjacentHTML('beforeend', `<div class="box easy">${i}</div>`)
 
         }
-    }else if(difficult==="medium"){
+    } else if (difficult === "medium") {
         document.getElementById('level').remove()
         document.querySelector('button').remove()
-    
-        document.getElementById('title').innerHTML="Difficoltà 2"
+
+        document.getElementById('title').innerHTML = "Difficoltà 2"
         for (let i = 1; i <= 81; i++) {
             document.getElementById('container').insertAdjacentHTML('beforeend', `<div class="box medium">${i}</div>`)
 
         }
-    }else if ((difficult==="difficult")){
+    } else if ((difficult === "difficult")) {
         document.getElementById('level').remove()
         document.querySelector('button').remove()
-    
-        document.getElementById('title').innerHTML="Difficoltà 3"
+
+        document.getElementById('title').innerHTML = "Difficoltà 3"
         for (let i = 1; i <= 49; i++) {
             document.getElementById('container').insertAdjacentHTML('beforeend', `<div class="box difficult">${i}</div>`)
 
         }
     }
-    
-    
+
+
 }
 
 
 
 document.querySelector('button').addEventListener('click', function () {
 
-   
+
     let userChoose = document.getElementById('level').value
-   
+
     if (userChoose === "easy") {
 
         difficultChooser("easy")
 
-        /* document.getElementById('level').remove()
-        document.querySelector('button').remove()
-    
-        document.getElementById('title').innerHTML="Difficoltà 1"
-        for (let i = 1; i <= 100; i++) {
-            
-            document.getElementById('container').insertAdjacentHTML('beforeend', `<div class="box easy">${i}</div>`)
 
-        } */
-    }else if(userChoose==="medium"){
-difficultChooser("medium")
+    } else if (userChoose === "medium") {
+        difficultChooser("medium")
 
-        /* document.getElementById('level').remove()
-        document.querySelector('button').remove()
-    
-        document.getElementById('title').innerHTML="Difficoltà 2"
-        for (let i = 1; i <= 81; i++) {
-            document.getElementById('container').insertAdjacentHTML('beforeend', `<div class="box medium">${i}</div>`)
-
-        } */
-    }else if ((userChoose==="difficult")){
+    } else if ((userChoose === "difficult")) {
         difficultChooser("difficult")
-        /* document.getElementById('level').remove()
-        document.querySelector('button').remove()
-    
-        document.getElementById('title').innerHTML="Difficoltà 3"
-        for (let i = 1; i <= 49; i++) {
-            document.getElementById('container').insertAdjacentHTML('beforeend', `<div class="box difficult">${i}</div>`)
 
-        } */
-    }else{
+    } else {
         alert('Scegli una difficoltà')
     }
 
