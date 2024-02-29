5ef713178dd939831f0139d25cfcdd13874b36fb
fraberret <f.berretta02@gmail.com>
2024-02-29 10:37:18 +0100

    fix when change difficult clear grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
 */
 /**in base alla scelta della difficolta, stampa una griglia
  * 
@@ -11,49 +12,45 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed 
 function difficultChooser(difficult) {
 
     if (difficult === "easy") {
-        document.getElementById('level').remove()
-        document.querySelector('button').remove()
-
+        //document.getElementById('level').remove()
+        //document.querySelector('button').remove()
+        document.getElementById('container').innerHTML = ''
         document.getElementById('title').innerHTML = "Facile"
         for (let i = 1; i <= 100; i++) {
 
             document.getElementById('container').insertAdjacentHTML('beforeend', `<div class="box easy">${i}</div>`)
 
         }
-    } else if (difficult === "medium") {
-        document.getElementById('level').remove()
-        document.querySelector('button').remove()
 
+    } else if (difficult === "medium") {
+        //document.getElementById('level').remove()
+        //document.querySelector('button').remove()
+        document.getElementById('container').innerHTML = ''
         document.getElementById('title').innerHTML = "Medio"
         for (let i = 1; i <= 81; i++) {
             document.getElementById('container').insertAdjacentHTML('beforeend', `<div class="box medium">${i}</div>`)
 
         }
     } else if ((difficult === "difficult")) {
-        document.getElementById('level').remove()
-        document.querySelector('button').remove()
-
+        //document.getElementById('level').remove()
+        //document.querySelector('button').remove()
+        document.getElementById('container').innerHTML = ''
         document.getElementById('title').innerHTML = "Difficile"
         for (let i = 1; i <= 49; i++) {
             document.getElementById('container').insertAdjacentHTML('beforeend', `<div class="box difficult">${i}</div>`)
 
         }
     }
-
-
 }
 
-
-
 document.querySelector('button').addEventListener('click', function () {
-
 
     let userChoose = document.getElementById('level').value
 
     if (userChoose === "easy") {
 
-        difficultChooser("easy")
 
+        difficultChooser("easy")
 
     } else if (userChoose === "medium") {
         difficultChooser("medium")
@@ -64,8 +61,6 @@ document.querySelector('button').addEventListener('click', function () {
     } else {
         alert('Scegli una difficoltà')
     }
-
-
 
 
     let boxElements = document.getElementsByClassName('box')
@@ -82,7 +77,5 @@ document.querySelector('button').addEventListener('click', function () {
 
         })
     }
-
-
 })
 
