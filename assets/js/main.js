0f7c9722de91689c18011b1e1f2a82eec7c30adc
fraberret <f.berretta02@gmail.com>
2024-02-29 12:25:31 +0100

    add 16 number generator

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,7 @@ Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
 
+
 */
 /**in base alla scelta della difficolta, stampa una griglia
  * 
@@ -79,3 +80,31 @@ document.querySelector('button').addEventListener('click', function () {
     }
 })
 
+
+/*Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: i funghi magici.
+Attenzione: nella stessa cella può essere posizionato al massimo un fungo, perciò nell’array dei funghi non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati
+- abbiamo calpestato una fungo
+- la cella si colora di rosso e la partita termina.
+Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una fungo o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono funghi).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una fungo.
+*/
+
+function getRndNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+
+  //Il computer deve generare 16 numeri casuali
+
+  let mushroomsArray=[]
+
+  while (mushroomsArray.length<16 ) {
+    
+    let randomNumber=getRndNumber(1,100)
+
+    if (!mushroomsArray.includes(randomNumber)) {
+        mushroomsArray.push(randomNumber)
+    }
+  }
+  console.log(mushroomsArray);
